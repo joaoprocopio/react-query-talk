7dcfd97ac21d8a5f7ebeeae35ffd51a3ce0aea91
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-01-25 18:27:20 -0300

    cors

--- src/exemplo-1/routes/_index.tsx
+++ src/exemplo-1/routes/_index.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react"
+
 export default function Exemplo1() {
+  const [frog, setFrog] = useState(undefined)
+
+  useEffect(() => {
+    const handleFetchRandomFrog = async () => {
+      setFrog(undefined)
+
+      const res = await fetch("https://frogs.media/api/random")
+      const json = await res.json()
+
+      setFrog(json)
+    }
+
+    handleFetchRandomFrog()
+  }, [])
+
+  console.log(frog)
+
   return <div>exemplo 1</div>
 }
