fbd287b0d272fd42d9f7e8c475bd7bce7c34c205
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-01-25 19:07:39 -0300

    vai

--- src/exemplo-1/routes/_index.tsx
+++ src/exemplo-1/routes/_index.tsx
@@ -1,23 +1,35 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 import * as FrogServices from "~/exemplo-1/services/frog"
 
 export default function Exemplo1() {
   const [frog, setFrog] = useState<FrogServices.Frog | undefined>(undefined)
 
+  const handleFetchRandomFrog = useCallback(async () => {
+    setFrog(undefined)
+
+    const frog = await FrogServices.getRandomFrog()
+
+    setFrog(frog)
+  }, [])
+
   useEffect(() => {
-    const handleFetchRandomFrog = async () => {
-      setFrog(undefined)
-
-      const frog = await FrogServices.getRandomFrog()
-
-      setFrog(frog)
-    }
-
     handleFetchRandomFrog()
   }, [])
 
-  console.log(frog)
+  return (
+    <div className="grid grid-rows-[2rem_24rem_4rem] gap-12">
+      <h1 className="font-bold text-3xl">{frog?.name}</h1>
 
-  return <div>exemplo 1</div>
+      <div className="flex gap-2">
+        <img src={frog?.url} className="object-cover w-96" />
+
+        {Array.isArray(frog?.aliases) && frog!.aliases!.map((alias) => <p>{alias}</p>)}
+      </div>
+
+      <button className="w-96 h-full border" onClick={handleFetchRandomFrog}>
+        me dá outro
+      </button>
+    </div>
+  )
 }
