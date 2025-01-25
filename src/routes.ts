9a132363a91a3459941a3fb380804ad1ce2dcb30
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-01-25 18:04:33 -0300

    module prefixes

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,17 @@ import type { RouteConfig } from "@react-router/dev/routes"
 import { prefix } from "@react-router/dev/routes"
 import { flatRoutes } from "@react-router/fs-routes"
 
-const getPrefixedRoutes = async (modulePrefix: string) => {
+type ModulePrefixes = "exemplo-1" | "exemplo-2"
+
+const getModulePrefixedRoutes = async (modulePrefix: ModulePrefixes) => {
   const routes = await flatRoutes({
     rootDirectory: `./${modulePrefix}/routes`,
   })
+  const prefixedRoutes = prefix(modulePrefix, routes)
 
-  return prefix(modulePrefix, routes)
+  return prefixedRoutes
 }
 
-const exemplo1ModulePrefix = "exemplo-1"
-const exemplo1Routes = await getPrefixedRoutes(exemplo1ModulePrefix)
+const exemplo1Routes = await getModulePrefixedRoutes("exemplo-1")
 
 export default [...exemplo1Routes] satisfies RouteConfig
